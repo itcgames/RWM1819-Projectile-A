e3e18a49200bbd2bfc2719b62ddf042c9b154d51
Pierce-James <C00203711@itcarlow.ie>
2019-01-24 16:57:09 +0000

    fixed manager class

--- Projectile.js
+++ Projectile.js
@@ -1,109 +1,144 @@
-class ProjectileManager
+
+class Projectile
 {
-    /**
-     * 
-     */
-    constructor()
+    constructor(s, type)
     {
-        this.projectiles = [];
-        this.globalGravity;
-        this.globalFriction;
-        this.globalAirResistance;
+        this.name = s;
+        this.x = 0;
+        this.y = 0; 
+        this.angle = 0;
+        this.speed = 0;
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.width = 5;
+        this.height = 5;
+        this.fired = false;
+        this.initialY = 0;
+        this.mX = 0;
+        this.mY = 0;
+        this.type = type;
     }
 
-    /**
-     * 
-     * @param {Projectile} p 
-     */
-    addProjectile(p)
+    update(g, f)
     {
-        console.log("Added projectile");
-        this.projectiles.push(p);
-    }
-
-    /**
-     * 
-     * @param {String : name} s 
-     */
-    createProjectile(s)
-    {
-        this.projectiles.push(new Projectile(s));
-    }
-
-    /**
-     * 
-     * @param {Friction Value} v 
-     */
-    setGlobalFriction(v)
-    {
-        this.globalFriction = v;
-    }
-
-    /**
-     * 
-     * @param {Gravity Value} v 
-     */
-    setGlobalGravity(v)
-    {
-        this.globalGravity = v;
-    }
-
-    setGlobalAirResistance(v)
-    {
-        this.globalAirResistance = v;
-    }
-
-    getProjectile(s)
-    {
-        for (var i = 0; i < this.projectiles.length; i++)
+        if (this.type === "simple")
         {
-            if (this.projectiles[i].name == s)
+            this.x += this.velocityX * this.speed;
+            this.y += this.velocityY * this.speed;
+        }
+        else if (this.type === "complex")
+        {
+            //Projectile Motion
+            var tempX = this.x;
+            var tempY = this.y;
+            var t = gameNs.game.dt / 1000;
+            //var t = dt / 1000;
+            this.x = tempX + this.velocityX * (t) * Math.cos(this.angle);
+            this.y = tempY + this.velocityY * (t) * Math.sin(this.angle) - ((g * (t * t) / 2));
+
+            this.velocityY += g;
+            this.velocityX -= f;
+
+            if (this.y < this.initialY)
             {
-                return this.projectiles[i];
+               
             }
-        }
-    }
-
-    fireProjectiles()
-    {
-        console.log("Fire Projectiles!");
-        for (var i = 0; i < this.projectiles.length; i++)
-        {
-            this.projectiles[i].fire();
-        }
-    }
-
-    update()
-    {
-        for (var i = 0; i < this.projectiles.length; i++)
-        {
-
-
-            if (this.projectiles[i].type === "simple")
+            else
             {
-                if (this.projectiles[i].IsFired())
-                {
-                    this.projectiles[i].update(this.globalGravity, this.globalFriction);
-                }
-            }
-            else{
-                //this.projectiles[i].setMousePosition(gameNs.game.mX, gameNs.game.mY);
-                if (this.projectiles[i].IsFired())
-                {
-                    this.projectiles[i].update(this.globalGravity, this.globalFriction);
-                }
+              //  this.velocityY = 0;
+              //  this.velocityX = 0;
             }
         }
+
     }
 
     render()
     {
-        for (var i = 0; i < this.projectiles.length; i++)
+        var canvas = document.getElementById('mycanvas');
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 8, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    setPosition(x, y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    IsFired()
+    {
+        return this.isFired;
+    }
+
+    setAngle(a)
+    {
+        this.angle = a;
+    }
+
+    getVelocity()
+    {
+        return new Vector2(this.velocityX / (1 / this.speed), this.velocityY / (1 / this.speed));
+    }
+
+    getPosition()
+    {
+        return new Vector2(this.x, this.y);
+    }
+
+    setMousePosition(mx, my)
+    {
+        this.mX = mx;
+        this.mY = my;
+    }
+
+    calculateAngle()
+    {
+        //(vx, vy) vector to mouse pointer
+        var vx = this.x - this.mX;
+        var vy = this.y - this.mY;
+
+        this.velocityX = -vx;
+        this.velocityY = -vy;
+
+        this.initialY = this.y;
+
+        console.log("x: " + this.velocityX + " Y: " +  this.velocityY);
+    }
+
+    setSpeed(s)
+    {
+        this.speed = s;
+    }
+
+    /**
+     * 
+     * @param {Float} v1 
+     * @param {Float} v2 
+     */
+    setVelocity(v1, v2)
+    {
+        this.velocityX = v1;
+        this.velocityY = v2;
+    }
+
+    setFired(c)
+    {
+        this.isFired = c;
+    }
+
+    fire()
+    {
+        if (this.type === "simple")
         {
-            if (this.projectiles[i].IsFired())
-            {
-                this.projectiles[i].render();
-            }
+            this.isFired = true;
+        }
+        else{
+            console.log("Fire!")
+            this.isFired = true;
+            this.calculateAngle();
         }
     }
 }
